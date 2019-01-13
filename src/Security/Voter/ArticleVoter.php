de1bdf1fb3d56da43ce35a9b7a809ae9af518bad
Olivier <olivier@toussaint.fr>
2019-01-13 20:07:27 +0100

    first update

---
+++ src/Security/Voter/ArticleVoter.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Security\Voter;
+
+use App\Entity\Article;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class ArticleVoter extends Voter
+{
+    public const EDIT = 'EDIT';
+    protected function supports($attribute, $subject)
+    {
+        return in_array($attribute, [self::EDIT])
+            && $subject instanceof Article;
+    }
+
+    protected function voteOnAttribute($attribute, $subject, TokenInterface $token)
+    {
+        $article = $subject;
+        $user = $token->getUser();
+
+        if (!$user instanceof UserInterface) {
+            return false;
+        }
+        if (!$article instanceof Article) {
+            return false;
+
+        }
+
+        switch ($attribute) {
+            case self::EDIT :
+                return  $article->getUser() === $user;
+                break;
+
+        }
+
+        return false;
+    }
+}
